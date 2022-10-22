190ccb9d22e58f7f749084a5ec02e63594cf74b2
Rafael Carrilho <carrilhorafael57@gmail.com>
2022-10-21 21:19:35 -0700

    models done

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Inventory;
+use App\Models\MinerStat;
+
 
 class User extends Authenticatable
 {
@@ -18,27 +21,24 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'wallet_addres',
+        'last_login'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
 
     /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
+     * Get the inventory associated with the user.
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function inventory()
+    {
+        return $this->hasOne(Inventory::class);
+    }
+
+    /**
+     * Get the miner stats associated with the user.
+     */
+    public function minerStats()
+    {
+        return $this->hasMany(MinerStat::class);
+    }
 }
